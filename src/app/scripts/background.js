d05ff49391617b7c217e8cecc348a441309e20b0
sushantkumr <10464008+sushantkumr@users.noreply.github.com>
2018-10-13 11:03:16 +0530

    Extracting values from form and sending it to server

--- src/app/scripts/background.js
+++ src/app/scripts/background.js
@@ -1,1 +1,8 @@
-console.log("background")
+/* globals chrome */
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.data.type == "sendTransaction")
+		console.log("Background script received data. Example to address: ",request.data.toAddress);
+      	sendResponse({message: "Sent values to Server"});
+    });
---
+++ test/index.js
@@ -0,0 +1,17 @@
+function submit() {
+    const fromAddress = document.getElementById('from').value;
+    const toAddress = document.getElementById('to').value;
+    const valueOfEth = document.getElementById('valueOfEth').value;
+    const gas = document.getElementById('gas').value;
+    const gasPrice = document.getElementById('gasPrice').value;
+    const data = document.getElementById('data').value;
+    const nonce = document.getElementById('nonce').value;
+    window.postMessage({ type: "SENDTRANSACTION", 
+                         fromAddress: fromAddress,
+                         toAddress: toAddress,
+                         valueOfEth: valueOfEth,
+                         gas: gas,
+                         gasPrice: gasPrice,
+                         data: data,
+                         nonce: nonce }, "*");
+};
