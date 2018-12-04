4e9327bf1cebd47c52b5b3bd0e76fbde78b67de6
milosristic007 <milosristic@bluedreampro.com>
2018-12-04 11:44:54 +0100

    Add some design to login

--- src/app/popup/Login.js
+++ src/app/popup/Login.js
@@ -1,109 +1,146 @@
 /* globals chrome */
 
-import React, { Component } from 'react';
-import '../../assets/css/App.css';
-import { Form, Button, Input, Message, Divider, Container } from 'semantic-ui-react'
-import logo from '../../assets/img/logo.png';
-import { Redirect} from 'react-router-dom';
-import { APICall } from './ajax';
+import React, { Component } from "react";
+import "../../assets/css/App.css";
+import {
+  Form,
+  Button,
+  Input,
+  Message,
+  Divider,
+  Container
+} from "semantic-ui-react";
+import appStore from "../../assets/img/AppStore.png";
+import playStore from "../../assets/img/PlayStore.png";
+import { Redirect } from "react-router-dom";
+import { APICall } from "./ajax";
 
 class App extends Component {
-
-  constructor (props) {
-      super(props)
-      this.state = {
-        errorMessage: '',
-        username: '',
-        loading: false,
-        redirect: false,
-      };
-  }
-
-  handleDismiss = () => {
-    this.setState({ errorMessage: '' })
-  }
-
-  // TODO change the parameters and URL and message
-  async sendUsernameToBackground() {
-    const params = {
-        username: this.state.username
+	constructor(props) {
+		super(props);
+		this.state = {
+			errorMessage: "",
+			username: "",
+			loading: false,
+			redirect: false
+		};
     }
-    const url = '/api/1/pair'
-    /*const response = */await APICall(url, params);
-    chrome.storage.sync.set({'username': this.state.username});
-  }
+    
+    componentDidMount() {
+        this.setState({
+            username: ''
+        });
+    }
 
+	handleDismiss = () => {
+		this.setState({ errorMessage: "" });
+	};
 
-  onSubmit = async (event) => {
-      event.preventDefault();
-      this.setState({ loading: true, errorMessage: '' });
+	// TODO change the parameters and URL and message
+	async sendUsernameToBackground() {
+		const params = {
+			username: this.state.username
+		};
+		const url = "/api/1/pair";
+		/*const response = */
+		await APICall(url, params);
+		chrome.storage.sync.set({ username: this.state.username });
+	}
+	
+	onSubmit = async event => {
+		event.preventDefault();
+        this.setState({ loading: true, errorMessage: "" });
+        try {
+            await this.sendUsernameToBackground();
+            this.setState({ redirect: true });
+        } catch (err) {
+            this.setState({ errorMessage: "Username does not exist" }); // Fix error message
+        }
+        
+        this.setState({ loading: false });
+    };
 
-      try {
-        await this.sendUsernameToBackground();
-        this.setState({ redirect: true});
-      } catch (err) {
-        this.setState({ errorMessage: "Username does not exist" });// Fix error message
-      }
+    render() {
+        const { redirect } = this.state;
 
-      this.setState({ loading: false });
-  }; 
+        if (redirect) {
+            return <Redirect to="/home" />;
+        }
 
-  render() {
-    const { redirect } = this.state;
-
-     if (redirect) {
-       return <Redirect to='/home' />;
-     }
-
-    return (
+        open = () => {
+            alert('Hello')
+        }
+        return (
         <div className="App">
-          <header>
-            <img src={logo} className="App-logo" alt="logo" />
-            <Divider hidden />
-            <p className="App-title">TapTrust Wallet</p>
-            <Divider hidden />
-          </header>
+            <header>
+                <div className="Title-Container">
+                    <p className="App-title">TapTrust Wallet</p>
+                </div>
+                <Divider hidden />
+            </header>
 
+            <p className="App-center">
+                First download the TapTrust Wallet 
+                mobile app and create an account.
+            </p>
+            
+            <div className="Appstore-Container">
+                <img src={appStore} className="AppStore-logo" alt="logo" />
+            </div>
 
-          <p className="App-center">Don't have a TapTrust account? Download the iOS/Android app to get started.</p>
+            <div className="Appstore-Container">
+                <img src={playStore} className="GooglePlay-logo" alt="logo" />
+            </div>
 
-          <p className="App-link"><u><a href="http://localhost:7080/" target="_blank" rel="noopener noreferrer">Learn more</a></u></p>
+            <p className="App-mid">
+                Enter your username below and approve the extension pairing request from the TapTrust Wallet mobile app.
+            </p>
 
-          <p className="App-center">Once you have a TapTrust account enter it below and authenticate the pairing on your mobile device</p>
-
-          <Container textAlign='center'>
-            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+            <Container textAlign="center">
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Divider hidden />
                 <Form.Field>
-                  <Input  placeholder='Username'
-                          transparent
-                          focus
-                          required
-                          value={this.state.username}
-                          onChange={event => this.setState({username: event.target.value})}
-                  />
-                <Divider fitted className='grey' />
-                </Form.Field>
-                <div>
-                {
-                  this.state.errorMessage ?
-                  <div>
-                    <Message  error
-                              size='mini'
-                              compact
-                              onDismiss={this.handleDismiss}
-                              content={this.state.errorMessage}
+                    <Input
+                        placeholder="Username"
+                        transparent
+                        focus
+                        required
+                        value={this.state.username}
+                        onChange={event =>
+                            this.setState({ username: event.target.value })
+                        }
                     />
-                    <Divider fitted hidden />
-                  </div>  : null
-                }
+                </Form.Field>
+                <p className="App-bottom">
+                    TapTrust will never request your Wallet password anywhere outside of the TapTrust Wallet mobile app.
+                </p>
+                <div>
+                    {this.state.errorMessage ? (
+                        <div>
+                        <Message
+                            error
+                            size="mini"
+                            compact
+                            onDismiss={this.handleDismiss}
+                            content={this.state.errorMessage}
+                        />
+                        <Divider fitted hidden />
+                        </div>
+                    ) : null}
                 </div>
-                <Button loading={this.state.loading} content='Request Authentication' circular />
+                <Button
+                    loading={this.state.loading}
+                    // content="Pair with TapTrust Wallet"
+                    circular
+                    size="large"
+                >
+                    <p className="PairButton">Pair with TapTrust Wallet</p>
+                </Button>
             </Form>
-          </Container>
+            </Container>
         </div>
-    );
-  }
+        );
+    }
 }
 
-export default (App);
+export default App;
